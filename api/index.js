d5dff16c38491e096251661e8dbb29c4347a3f84
Gilang Risky M <gilangriskymahardhika@gmail.com>
2025-03-23 16:13:00 +0700

    update api/index.js

--- api/index.js
+++ api/index.js
@@ -1,5 +1,5 @@
-import app from "../src/server";
+const app = require("../src/server");
 
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
   return app(req, res);
-}
+};
